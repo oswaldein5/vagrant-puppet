a123cf0af8c5252c71a2e522ac59e22374b9e2da
oswaldeins <oswaldeins.ec@gmail.com>
2024-05-06 12:24:21 -0500

    update manifest

--- manifests/default.pp
+++ manifests/default.pp
@@ -5,7 +5,13 @@ $port = '8080'
 $email = 'oswaldo.solano@mail.com'
 $user_root = 'root'
 $wp_admin = 'wp_admin'
+
+# Passwords should NOT be put in plain text within the manifests, use external modules such as 'hiera' to handle sensitive information
+# For the demonstration, a key is used without any form of encryption
 $password = 'pass.123'
+# NO se deben poner los passwords en texto plano dentro de los manifiestos, usar módulos externos como 'hiera' para el manejo de información sensible
+# Para efecto de la demostración se usa una clave sin ninguna forma de cifrado
+
 $db_name = 'wordpress'
 $db_user = 'wordpress'
 $db_host = 'localhost'
